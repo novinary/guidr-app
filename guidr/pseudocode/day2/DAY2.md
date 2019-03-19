f9d7bbd0269c7352a19bd4416bbdc76196019e93
novinary <novina.dev@gmail.com>
2019-03-19 15:09:27 +0000

    Start using Redux with React

--- guidr/pseudocode/day2/DAY2.md
+++ guidr/pseudocode/day2/DAY2.md
@@ -6,3 +6,15 @@ Step 2
 Create new files for the components - Done 
 Setup imports, component, default export - Done
 Import into AppRouter so they can be used - Done
+Create a separate component for Header - Done
+Make editTrip link dynamic - Done
+
+Step 3
+** Start using Redux with React **  
+Notes: Connect the components to the redux store so they can
+read off of the store so they can fetch values from the store
+and render those.
+If someone interacts with the user interface maybe the a add a new trip or
+change the filters 
+
+
--- guidr/src/routers/AppRouter.js
+++ guidr/src/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import GuidrAppDash from '../components/Dashboard/GuidrAppDash'
 import FilterTripType from '../components/Dashboard/FilterTripType'
 import AddTrip from '../components/Profile/AddTrip'
@@ -15,7 +15,7 @@ const AppRouter = () => (
         <Route path="/" component={GuidrAppDash} exact={true} />
         <Route path="/tripType" component={FilterTripType} />
         <Route path="/addTrip" component={AddTrip} />
-        <Route path="/editTrip" component={EditTrip} />
+        <Route path="/editTrip/:id" component={EditTrip} />
         <Route path="/viewFeed" component={AdventureFeed} />
       </Switch>
     </div>
