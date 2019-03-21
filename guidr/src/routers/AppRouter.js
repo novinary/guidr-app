7c4beb7eacd568430d4859b3160df9dff6dba65b
novinary <novina.dev@gmail.com>
2019-03-21 14:03:19 +0000

    Create a login UI

--- guidr/src/routers/AppRouter.js
+++ guidr/src/routers/AppRouter.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import GuidrAppDash from '../components/Dashboard/GuidrAppDash'
-import AddTrip from '../components/Dashboard/AddTrip'
-import EditTrip from '../components/Dashboard/EditTrip'
-import Header from '../components/Header'
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import GuidrAppDash from "../components/Dashboard/GuidrAppDash";
+import AddTrip from "../components/Dashboard/AddTrip";
+import EditTrip from "../components/Dashboard/EditTrip";
+import Header from "../components/Header";
+import { LoginPage } from "../components/Dashboard/LoginPage";
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={GuidrAppDash} exact={true} />
+        <Route path="/" component={LoginPage} exact={true} />
+        <Route path="/dashboard" component={GuidrAppDash} />
         <Route path="/addTrip" component={AddTrip} />
         <Route path="/editTrip/:id" component={EditTrip} />
       </Switch>
